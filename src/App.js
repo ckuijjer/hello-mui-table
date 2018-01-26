03af743278631b607b9e03aca7d67426e30b2270
Vijai Ramcharan <vijai.ramcharan@gmail.com>
2018-01-26 16:34:08 +0100

    added filter on name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ const CSV_URL = 'https://s3-eu-west-1.amazonaws.com/kuijjer-public/sample_data.c
 class App extends Component {
   state = {
     loading: true,
-    data: null
+    data: null,
+    query: ''
   }
 
   componentDidMount() {
@@ -22,20 +23,42 @@ class App extends Component {
     });
   }
 
+  onQueryChanged = (event) => {
+    this.setState({query:event.target.value})
+  }
   render() {
     if (this.state.loading) {
       return <Loading />;
     };
 
-    const data = this.state.data
+    const {data, query} = this.state;
     const columns = this.state.data.columns.map(column => ({ name: column }));
+    let filteredData = data;
+    const startTime = +new Date();
+    if(query && query.length > 0) {
+      filteredData = data.filter( row => { 
+        const first = row.first || '';
+        const last = row.last || '';
+        const lowerCaseQuery = query.toLowerCase();
+        return first.toLowerCase().includes(query.toLowerCase())
+          || last.toLowerCase().includes(query.toLowerCase());
+      });
+    }
+    const filterTime = (+new Date()) - startTime;
 
     return (
       <div style={{ height: '100vh' }}>
+        <div style={{padding: 20}}>
+         <span style={{fontSize: 10, marginRight: 16}}>Name (first or last)</span>
+         <span><input type="text" value={this.state.query} onChange={this.onQueryChanged} /></span>
+        </div>
+        <div style={{padding: 20}}>
+         <span style={{fontSize: 10, marginRight: 16}}>{`showing ${filteredData.length} records (filter took ${filterTime} ms)`}</span>
+        </div>
         <ParentSize>
           {({ width, height }) => (
             <MuiTable
-              data={data}
+              data={filteredData}
               columns={columns}
               width={width}
               height={height}
